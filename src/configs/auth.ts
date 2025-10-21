a38339302dbeac11b69cdf8a3564d1bc41985f20
Vitorcd20 <vitorvieeira12008@gmail.com>
2025-10-21 18:16:41 -0300

    feat: user token

---
+++ src/configs/auth.ts
@@ -0,0 +1,6 @@
+export const authConfig = {
+  jwt: {
+    secret: "vitor",
+    expiresIn: "1d",
+  },
+};
--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,31 +1,42 @@
 import { Request, Response } from "express";
-import z from "zod";
-import {prisma} from '@/database/prisma'
+import { prisma } from "@/database/prisma";
 import { AppError } from "@/utils/app-error";
 import { compare } from "bcrypt";
+import z from "zod";
+import { authConfig } from "@/configs/auth";
+import { sign } from "jsonwebtoken";
 
 class SessionsController {
   async create(request: Request, response: Response) {
     const bodySchema = z.object({
       email: z.string().email({ message: "Email is not valid" }),
-      password: z.string()
+      password: z.string(),
     });
 
     const { email, password } = bodySchema.parse(request.body);
 
-    const user = await prisma.user.findFirst({where: {email}})
+    const user = await prisma.user.findFirst({ where: { email } });
 
-    if(!user) {
-      throw new AppError("Email or password is not valid", 401)
+    if (!user) {
+      throw new AppError("Email or password is not valid", 401);
     }
 
-    const passwordMatched = await compare(password, user.password)
+    const passwordMatched = await compare(password, user.password);
 
-    if(!passwordMatched) {
-        throw new AppError("Email or password is not valid", 401)
+    if (!passwordMatched) {
+      throw new AppError("Email or password is not valid", 401);
     }
 
-    response.json({ email, password });
+    const {expiresIn, secret} = authConfig.jwt
+
+    const token = sign({role: user.role}, secret, {
+        subject: user.id,
+        expiresIn
+    })
+
+    const {password: _, ...userWithoutPassword } = user
+
+    response.json({ token, user: userWithoutPassword});
   }
 }
 
