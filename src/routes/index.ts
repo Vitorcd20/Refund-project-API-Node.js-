036585a1803ca2f1c4d3bfbf645b9a118e9857f4
Vitorcd20 <vitorvieeira12008@gmail.com>
2025-10-21 20:31:45 -0300

    feat: ensure-authenticated

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,8 @@ import { usersRoutes } from "./users-routes";
 import { sessionsRoutes } from "./sessions-routes";
 import { refundsRoutes } from "./refunds-routes";
 
+import { ensureAuthenticated } from "@/middlewares/ensure-autheticated";
+
 const routes = Router();
 
 //public routes
@@ -11,6 +13,7 @@ routes.use("/users", usersRoutes);
 routes.use("/sessions", sessionsRoutes);
 
 //private routes
+routes.use(ensureAuthenticated)
 routes.use("/refunds", refundsRoutes)
 
 export { routes };
