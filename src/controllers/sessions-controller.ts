c6b218fdd79c5fe0622b30fcaad5997efb047c15
Vitorcd20 <vitorvieeira12008@gmail.com>
2025-10-21 18:12:36 -0300

    feat: checking if user exist

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from "express";
 import z from "zod";
+import {prisma} from '@/database/prisma'
+import { AppError } from "@/utils/app-error";
+import { compare } from "bcrypt";
 
 class SessionsController {
   async create(request: Request, response: Response) {
@@ -9,6 +12,18 @@ class SessionsController {
     });
 
     const { email, password } = bodySchema.parse(request.body);
+
+    const user = await prisma.user.findFirst({where: {email}})
+
+    if(!user) {
+      throw new AppError("Email or password is not valid", 401)
+    }
+
+    const passwordMatched = await compare(password, user.password)
+
+    if(!passwordMatched) {
+        throw new AppError("Email or password is not valid", 401)
+    }
 
     response.json({ email, password });
   }
