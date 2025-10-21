b7b3c7e605124397feba8199a70773b38aecefc3
Vitorcd20 <vitorvieeira12008@gmail.com>
2025-10-21 16:22:54 -0300

    feat: validating users data

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import 'express-async-errors'
 import cors from "cors";
 
 import { routes } from "./routes";
 import { errorHandling } from "@/middlewares/error-handling";
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,9 +1,28 @@
-import { Request, Response } from "express"
+import { Request, Response } from "express";
+import { UserRole } from "@prisma/client";
+import z from "zod";
 
 class UsersController {
- async create(request: Request, response: Response) {
-    response.json({message: 'ok'})
- }
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      name: z.string().trim().min(2, { message: "Name is required" }),
+      email: z
+        .string()
+        .trim()
+        .email({ message: "Email is not valid" })
+        .toLowerCase(),
+      password: z
+        .string()
+        .min(6, { message: "password must have at least 6 characters" }),
+      role: z
+        .enum([UserRole.employe, UserRole.manager])
+        .default(UserRole.employe),
+    });
+
+    const { name, email, password, role } = bodySchema.parse(request.body);
+
+    response.json({ name, email, password, role });
+  }
 }
 
-export {UsersController}
+export { UsersController };
