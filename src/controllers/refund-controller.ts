11512e9cb16e046ac300b45d518469b6efd6910e
Vitorcd20 <vitorvieeira12008@gmail.com>
2025-10-22 10:53:02 -0300

    feat: request data validation

--- src/controllers/refund-controller.ts
+++ src/controllers/refund-controller.ts
@@ -1,11 +1,29 @@
-
 import { Request, Response } from "express";
+import z from "zod";
 
+const CategoriesEnum = z.enum([
+  "food",
+  "others",
+  "services",
+  "transport",
+  "accommodation",
+]);
 
 class RefundsController {
   async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      name: z.string().trim().min(1, { message: "Enter the request name" }),
+      category: CategoriesEnum,
+      amount: z
+        .number()
+        .positive({ message: "The value needs to be positive" }),
+      filename: z.string().min(20),
+    });
 
-    response.json({message: 'ok'});
+
+    const {amount, category, filename, name} = bodySchema.parse(request.body) 
+
+    response.json({ message: "ok" });
   }
 }
 
