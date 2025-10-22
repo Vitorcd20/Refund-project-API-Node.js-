6f3150fd3403da208bbaf606290a5641dc6aab91
Vitorcd20 <vitorvieeira12008@gmail.com>
2025-10-22 14:04:38 -0300

    feat: implement pagination

--- src/controllers/refund-controller.ts
+++ src/controllers/refund-controller.ts
@@ -22,11 +22,10 @@ class RefundsController {
       filename: z.string().min(20),
     });
 
+    const { name, category, amount, filename } = bodySchema.parse(request.body);
 
-    const {name, category, amount, filename} = bodySchema.parse(request.body) 
-
-    if(!request.user?.id) {
-      throw new AppError("Unauthorized", 401)
+    if (!request.user?.id) {
+      throw new AppError("Unauthorized", 401);
     }
 
     const refund = await prisma.refunds.create({
@@ -35,33 +34,59 @@ class RefundsController {
         category,
         amount,
         filename,
-        userId: request.user.id
-      }
-    })
+        userId: request.user.id,
+      },
+    });
 
-    response.status(201).json(refund)
+    response.status(201).json(refund);
   }
 
-  async index(request: Request, response: Response){
+  async index(request: Request, response: Response) {
     const querySchema = z.object({
-      name: z.string().optional().default("")
-    })
+      name: z.string().optional().default(""),
+      page: z.coerce.number().optional().default(1),
+      perPage: z.coerce.number().optional().default(10),
+    });
 
-    const {name} = querySchema.parse(request.query)
+    const { name, page, perPage } = querySchema.parse(request.query);
 
+    const skip = (page - 1) * perPage;
 
     const refunds = await prisma.refunds.findMany({
+      skip,
+      take: perPage,
       where: {
         user: {
           name: {
-            contains: name.trim()
-          }
-        }
+            contains: name.trim(),
+          },
+        },
       },
-      orderBy: {createdAt: "desc"},
-      include: {user: true}
-    })
-    response.json(refunds)
+      orderBy: { createdAt: "desc" },
+      include: { user: true },
+    });
+
+    const totalRecords = await prisma.refunds.count({
+      where: {
+        user: {
+          name: {
+            contains: name.trim(),
+          },
+        },
+      },
+    });
+
+    const totalPages = Math.ceil(totalRecords / perPage);
+
+    response.json({
+      refunds,
+      pagination: {
+        page,
+        perPage,
+        totalRecords,
+        totalPages: totalPages > 0 ? totalPages : 1,
+      },
+    });
   }
 }
 
