eeccc3016489ae063c6c50a6aa9eb1ca90b8fe86
Vitorcd20 <vitorvieeira12008@gmail.com>
2025-10-22 16:17:21 -0300

    feat: manipulating file

--- src/controllers/uploads-controller.ts
+++ src/controllers/uploads-controller.ts
@@ -1,14 +1,18 @@
 import { Request, Response } from "express";
-import z from "zod";
+import z, { ZodError } from "zod";
 import uploadConfig from "@/configs/upload";
+import { DiskStorage } from "@/providers/disk-storage";
+import { AppError } from "@/utils/app-error";
 
 class UploadsController {
   async create(request: Request, response: Response) {
+    const diskStorage = new DiskStorage();
+
     try {
       const fileSchema = z
         .object({
-          filename: z.string().min(1, { message: "File is required" }),
-          mimeType: z
+          filename: z.string().min(1, "File is required"),
+          mimetype: z 
             .string()
             .refine(
               (type) => uploadConfig.ACCEPTED_IMAGE_TYPES.includes(type),
@@ -24,11 +28,22 @@ class UploadsController {
         })
         .passthrough();
 
-      const { file } = fileSchema.parse(request.file);
+      const file = fileSchema.parse(request.file);
+      const filename = await diskStorage.saveFile(file.filename);
 
-      response.json({ message: "ok" });
+      return response.json({ filename }); 
 
-    } catch (error) {}
+    } catch (error) {
+      if (request.file) {
+        await diskStorage.deleteFile(request.file.filename, "tmp");
+      }
+
+      if (error instanceof ZodError) {
+        throw new AppError(error.issues[0].message);
+      }
+      
+      throw error;
+    }
   }
 }
 
